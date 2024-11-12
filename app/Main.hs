2c3580cca925c434849e8e62288ff14ed7454d22
JktuJQ <odmamontov@gmail.com>
2024-11-12 21:14:21 +0300

    Created `PINN.Testing` submodule
    Created `PINN.Testing` submodule which contains some tests of neural network.

--- app/Main.hs
+++ app/Main.hs
@@ -1,77 +1,11 @@
 module Main where
 
-import Data.Bifunctor
-import System.Random
-
-import Graphics.EasyPlot
-
-import Data.Vector ((!))
-import qualified Data.Vector as V
-
-import qualified Data.Matrix as M
-
-import Task (differentialEquations)
-
-import CauchyProblem (CauchyData(..))
-import CauchyProblem.Times (timeline, TimeSettings(..), fromTimeSettings)
-import CauchyProblem.NumericalMethods.Methods (methodRungeKutta)
-
-import PINN.Model
-import PINN.DifferentiableFns
-import PINN.Optimisers
-import PINN.Training
+import PINN.Testing
 
 main :: IO ()
 main = do
-    -- XOR approximation
-    putStrLn "XOR approximation"
-    let dataset = V.fromList [(V.fromList [0.0, 0.0], V.singleton 0.0), (V.fromList [1.0, 0.0], V.singleton 1.0), (V.fromList [0.0, 1.0], V.singleton 1.0), (V.fromList [1.0, 1.0], V.singleton 0.0)]
-    let model = assembleModel 2 [
-            (2, const $ const 1.0, const $ const 0.0, Sin),
-            (1, const $ const 1.0, const $ const 0.0, Sin)
-                                ]
-    let sgd = SGD 0.5 False
-    let (trained_model, losses, _) = train model sgd
-                                (dataset, mkStdGen 1)
-                                (TrainingHyperparameters 100 (const 0.01) SSR 4)
-    _ <- plot (PNG "tests/losses_xor.png") (Data2D [Title "loss", Style Lines] [] [(fromIntegral i, losses ! i) | i <- [0..(V.length losses - 1)]])
+    let (_, xor) = testXOR
+    let (_, x_squared) = testXSquared
+    let (_, trigonometry) = testTrigonometry
 
-    -- x^2 approximation
-    putStrLn "x2 approximation"
-    let timegrid = fromTimeSettings (TimeSettings (-1.0) 1.0 (const 0.1))
-    let cauchy_data = CauchyData (V.fromList [1.0]) (V.fromList [\(t, _) -> 2.0 * t])
-    let result = methodRungeKutta timegrid cauchy_data
-    let dataset = V.map (\t -> (V.singleton t, V.singleton (t * t))) (V.fromList $ timeline timegrid)
-
-    let model = assembleModel 1 [
-            (1, const $ const 1.0, const $ const 0.0, Sin),
-            (1, const $ const 1.0, const $ const 0.0, Id)
-                                ]
-    let sgd = SGD 0.5 False
-    let (trained_model, losses, _) = train model sgd
-                                (dataset, mkStdGen 1)
-                                (TrainingHyperparameters 1000 (const 0.01) SSR 1)
-    _ <- plot (PNG "tests/predicted_x2.png") (Data2D [Title "predicted", Style Lines] [] [(i, V.head $ predict trained_model (V.singleton i)) | i <- [-1.0, -0.9..1.0]])
-    _ <- plot (PNG "tests/losses_x2.png") (Data2D [Title "loss", Style Lines] [] [(fromIntegral i, losses ! i) | i <- [0..(V.length losses - 1)]])
-
-    -- sin(2.0 * cos(x) + 3.0) + 2.5 approximation
-    putStrLn "sin(2.0 * cos(x) + 3.0) + 2.5 approximation"
-    let timegrid = fromTimeSettings (TimeSettings (-(2.0 * pi)) (2.0 * pi) (const 0.1))
-    let cauchy_data = CauchyData (V.fromList [1.54]) (V.fromList [\(t, _) -> (-(2.0 * sin t * cos (2.0 * cos t + 3.0)))])
-    let result = methodRungeKutta timegrid cauchy_data
-    let dataset = V.map (\t -> (V.singleton t, V.singleton (sin(2.0 * cos t + 3.0) + 2.5))) (V.fromList $ timeline timegrid)
-
-    let model = assembleModel 1 [
-            (1, const $ const 1.0, const $ const 0.0, Sin),
-            (1, const $ const 1.0, const $ const 0.0, Sin),
-            (1, const $ const 1.0, const $ const 0.0, Sin),
-            (1, const $ const 1.0, const $ const 0.0, Id)
-                                ]
-    let sgd = SGD 0.0 False
-    let (trained_model, losses, _) = train model sgd
-                                (dataset, mkStdGen 1)
-                                (TrainingHyperparameters 1000 (const 0.001) SSR 1)
-    _ <- plot (PNG "tests/predicted_sincos_trig_fn.png") (Data2D [Title "predicted", Style Lines] [] [(i, V.head $ predict trained_model (V.singleton i)) | i <- [-(2.0 * pi), (-(2.0 * pi) + 0.1)..(2.0 * pi)]])
-    _ <- plot (PNG "tests/losses_sincos_trig_fn.png") (Data2D [Title "loss", Style Lines] [] [(fromIntegral i, losses ! i) | i <- [0..(V.length losses - 1)]])
-
-    return ()
+    mapM_ (\(f, p) -> f p) (zip (map plotTest ["xor", "x2", "trigonometry"]) [xor, x_squared, trigonometry])
---
+++ app/PINN/Testing.hs
@@ -0,0 +1,120 @@
+{-
+    `PINN.Testing` submodule provides several tests for neural networks.
+    Most of the time those tests try to approximate some functions with different neural network configurations.
+-}
+module PINN.Testing where
+
+import Data.Maybe
+import System.Random
+
+import Graphics.EasyPlot
+
+import Data.Vector ((!))
+import qualified Data.Vector as V
+
+import PINN.Model
+import PINN.DifferentiableFns
+import PINN.Optimisers
+import PINN.Training
+
+{-
+    `TestPlots` record datatype holds several plots that test may or may not provide.
+
+    It is needed to pack those plots and then supply them to the `plotTest` function.
+-}
+data (Plot a) => TestPlots a = TestPlots {
+    {-
+        Original function (analytic plot).
+    -}
+    original :: Maybe a,
+    {-
+        Predicted function (plot of predicted values).
+    -}
+    prediction :: Maybe a,
+    {-
+        Plot of neural networks losses.
+    -}
+    losses :: Maybe a
+}
+{-
+    `plotTest` function plots all data from `TestPlots` datatype.
+
+    If present, plots will be named as follows: 'tests/{plot_type (original, prediction, losses)}_{plot_name}.png'.
+-}
+plotTest :: (Plot a) => String -> TestPlots a -> IO ()
+plotTest name (TestPlots orig pr loss) = do
+    let plot_names = ["tests/" ++ plot_name ++ "_" ++ name ++ ".png" | plot_name <- ["original", "predicted", "losses"]]
+    _ <- mapM_ (\(str, p) -> if isJust p then plot (PNG str) (fromJust p) else return False) (zip plot_names [orig, pr, loss])
+    return ()
+
+{-
+    `testXOR` function tries to approximate `XOR` function (0 0 -> 0, 0 1 -> 1, 1 0 -> 1, 1 1 -> 0).
+-}
+testXOR :: (SequentialModel, TestPlots (Graph2D Double Double))
+testXOR = (trained_model, TestPlots Nothing Nothing (Just losses_plot))
+ where
+    dataset = V.fromList [
+        (V.fromList [0.0, 0.0], V.singleton 0.0),
+        (V.fromList [1.0, 0.0], V.singleton 1.0),
+        (V.fromList [0.0, 1.0], V.singleton 1.0),
+        (V.fromList [1.0, 1.0], V.singleton 0.0)
+                         ]
+    
+    model = assembleModel 2 [
+            (1, const $ const 1.0, const $ const 0.0, Sin),
+            (1, const $ const 1.0, const $ const 0.0, Sin)
+                            ]
+    
+    sgd = SGD 0.5 False
+    hyperparams = TrainingHyperparameters 100 (const 0.01) SSR 4
+    (trained_model, loss, _) = train model sgd (dataset, mkStdGen 1) hyperparams
+        
+    losses_plot = Data2D [Title "loss", Style Lines] [] [(fromIntegral i, loss ! i) | i <- [0..(V.length loss - 1)]]
+
+{-
+    `testXSquared` function tries to approximate `x^2` function.
+-}
+testXSquared :: (SequentialModel, TestPlots (Graph2D Double Double))
+testXSquared = (trained_model, TestPlots (Just original_plot) (Just predicted_plot) (Just losses_plot))
+ where
+    range = [(-1.0),(-0.9)..1.0]
+
+    dataset = V.map (\t -> (V.singleton t, V.singleton (t * t))) (V.fromList range)
+
+    model = assembleModel 1 [
+            (1, const $ const 1.0, const $ const 0.0, Sin),
+            (1, const $ const 1.0, const $ const 0.0, Id)
+                            ]
+    
+    sgd = SGD 0.5 False
+    hyperparams = TrainingHyperparameters 1000 (const 0.01) SSR 1
+    (trained_model, loss, _) = train model sgd (dataset, mkStdGen 1) hyperparams
+
+    original_plot = Function2D [Title "predicted", Style Lines] [For range] (\x -> x * x)
+    predicted_plot = Data2D [Title "predicted", Style Lines] [Range (-1.0) 1.0] [(i, V.head $ predict trained_model (V.singleton i)) | i <- range]
+    losses_plot = Data2D [Title "loss", Style Lines] [] [(fromIntegral i, loss ! i) | i <- [0..(V.length loss - 1)]]
+
+{-
+    `testTrigonometry` function tries to approximate `sin(2.0 * cos(x) + 3.0) + 2.5` function.
+-}
+testTrigonometry :: (SequentialModel, TestPlots (Graph2D Double Double))
+testTrigonometry = (trained_model, TestPlots (Just original_plot) (Just predicted_plot) (Just losses_plot))
+ where
+    range = [(-(2.0 * pi)),((-(2.0 * pi)) + 0.1)..(2.0 * pi)]
+
+    dataset = V.map (\t -> (V.singleton t, V.singleton (sin(2.0 * cos t + 3.0) + 2.5))) (V.fromList range)
+
+    model = assembleModel 1 [
+            (1, const $ const 1.0, const $ const 0.0, Sin),
+            (1, const $ const 1.0, const $ const 0.0, Sin),
+            (1, const $ const 1.0, const $ const 0.0, Sin),
+            (1, const $ const 1.0, const $ const 0.0, Id)
+                            ]
+    
+    sgd = SGD 0.0 False
+    hyperparams = TrainingHyperparameters 1000 (const 0.001) SSR 1
+    (trained_model, loss, _) = train model sgd (dataset, mkStdGen 1) hyperparams
+
+    original_plot = Function2D [Title "predicted", Style Lines] [Range (-(2.0 * pi)) (2.0 * pi)] (\x -> sin (2.0 * cos x + 3.0) + 2.5)
+    predicted_plot = Data2D [Title "predicted", Style Lines] [Range (-(2.0 * pi)) (2.0 * pi)] [(i, V.head $ predict trained_model (V.singleton i)) | i <- range]
+    losses_plot = Data2D [Title "loss", Style Lines] [] [(fromIntegral i, loss ! i) | i <- [0..(V.length loss - 1)]]
