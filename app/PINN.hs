bc9b86ca029ffa555fe253715b1764a0b0ea1ffa
JktuJQ <odmamontov@gmail.com>
2024-12-23 16:08:37 +0300

    Deleted all Haskell-related files
    Project was remade in Python (using PyTorch). Python files will be uploaded in the next commit.

--- app/PINN.hs
+++
@@ -1,5 +0,0 @@
-{-
-    `PINN` module contain several submodules which all implement neural networks.
-    This module itself defines only PINN-specific things, like PINN loss function.
--}
-module PINN where
